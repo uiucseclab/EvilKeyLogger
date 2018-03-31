0c7cf02cc6342a74918295a0f06e46164c856359
Benjamin Pollak <benjaminwpollak@gmail.com>
2018-03-31 17:47:11 -0500

    merged master

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-from pynput.keyboard import Key, Listener
-import logging
-
-class Logger:
-    def __init__(self, log_name):
-        self.log_name = log_file
-        self.num_times_enter_pressed = 0
-    
-    def on_press(self, key):
-        myChar = str(key)
-        if(myChar == "Key.enter"):
-            # TODO: close the file
-            self.num_times_enter_pressed+=1
-            if(self.num_times_enter_pressed == 2):
-                return False
-        logging.info(str(key))
-	
-    def log_keystrokes(self):
-        logging.basicConfig(filename=(self.log_name), level=logging.DEBUG, format='%(message)s')
-        #logging.basicConfig(filename=(self.log_name), level=logging.DEBUG, format='%(asctime)s: %(message)s')
-        
-        with Listener(on_press=self.on_press) as listener:
-            listener.join()
-
+import Logger
 if __name__ == '__main__':
     log_file = 'C:\\Users\\Benjamin Pollak\\Desktop\\log.txt'
-    logger = Logger(log_file)
+    logger = Logger.Logger(log_file)
     logger.log_keystrokes()
